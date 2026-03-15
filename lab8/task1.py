9faf335fa72fb87d7a7be8305379a7fbe6afd024
Ivan Platunov <ivankubik2007@gmail.com>
2026-03-15 17:43:37 +0300

    final commit

--- lab8/task1.py
+++ lab8/task1.py
@@ -1,7 +1,7 @@
 import cv2
 
 image = cv2.imread("lab8/variant-3.jpeg")
-hsv_im = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-cv2.imshow('HSV image',hsv_im)
+hsv_im = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # type: ignore
+cv2.imshow("HSV image", hsv_im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- lab8/task2-3.py
+++ lab8/task2-3.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    if not ret: break
-    h,w,_ = frame.shape
+    if not ret:
+        break
+    h, w, _ = frame.shape
     cx_frame = w // 2
     cy_frame = h // 2
     x1 = cx_frame - 100
@@ -12,23 +14,25 @@ while True:
     x2 = cx_frame + 100
     y2 = cy_frame + 100
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    mask = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    mask = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+    )
     cont, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     center = None
     if cont:
         largest_cont = max(cont, key=cv2.contourArea)
         M = cv2.moments(largest_cont)
         if M["m00"] > 0:
-            cx = int(M["m10"]/M["m00"])
-            cy = int(M["m01"]/M["m00"])
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
             center = (cx, cy)
-            cv2.circle(frame,center,5,(0,255,255),-1)
-    if center != None and x1<center[0]<x2 and y1<center[1]<y2:
-        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),3)
-    else: 
-         cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),3)
+            cv2.circle(frame, center, 5, (0, 255, 255), -1)
+    if center != None and x1 < center[0] < x2 and y1 < center[1] < y2:
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    else:
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
     cv2.imshow("Image", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
